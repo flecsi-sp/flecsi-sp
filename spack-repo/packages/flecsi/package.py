0bff2d3e5ef584658c3db3016a761794d5cb7a42
Ben Bergen <bergen@lanl.gov>
2024-05-26 21:27:05 +0000

    Fixed Spack Package

--- spack-repo/packages/flecsi/package.py
+++ spack-repo/packages/flecsi/package.py
@@ -6,3 +6,4 @@ class Flecsi(Flecsi):
     Additional named versions for FleCSI.
     """
     version("2.3-beta", commit="45050e422eb85b0d6bfd81cb2c40f9b1d3f9ca85")
+    patch("get_axis.patch", when="@2.3-beta")
