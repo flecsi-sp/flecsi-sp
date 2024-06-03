5a7adf4370bf7f4d286ac2f31e22c05b93177a01
Ben Bergen <bergen@lanl.gov>
2024-06-03 16:55:45 +0000

    Added flecsi@2.3-narray target

--- spack-repo/packages/flecsi/package.py
+++ spack-repo/packages/flecsi/package.py
@@ -5,5 +5,6 @@ class Flecsi(Flecsi):
     """
     Additional named versions for FleCSI.
     """
+    version("2.3-narray", commit="5b814a9efbe9022defd0a631a0b903f265e98d12")
     version("2.3-beta", commit="45050e422eb85b0d6bfd81cb2c40f9b1d3f9ca85")
     patch("get_axis.patch", when="@2.3-beta")
